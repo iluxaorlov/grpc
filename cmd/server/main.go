9c00feec764ce9b2fd7896f1f27b59fae644ed62
Ilya Orlov <iluxaorlov@yandex.ru>
2020-07-22 01:00:48 +0300

    added grpc server

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/iluxaorlov/grpc/pkg/adder"
+	"github.com/iluxaorlov/grpc/pkg/api"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	s := grpc.NewServer()
+	srv := &adder.GRPCServer{}
+	api.RegisterAdderServer(s, srv)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+}
